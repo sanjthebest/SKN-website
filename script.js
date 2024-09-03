f72ca5766c1d68f81833bccc9792e7f6ecf09af3
Sanjana Robbi <158380192+sanjthebest@users.noreply.github.com>
2024-09-03 17:23:28 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+const menuIcon = document.querySelector('#menu-icon');
+const navbar = document.querySelector('.navbar');
+
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active')
+}
+
+
